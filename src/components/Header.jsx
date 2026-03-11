ad370176f96a564f88dc69474a979f1662e3fb39
Dan Zerbib <danzerbib@gmail.com>
2026-03-11 19:25:30 +0100

    added link to home page on logo in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router';
 import logo from '../assets/logo-vinted.png';
 import { HiMagnifyingGlass } from 'react-icons/hi2';
 
@@ -5,7 +6,9 @@ const Header = ({ search, setSearch }) => {
     return (
         <header>
             <div className="container">
-                <img src={logo} alt="Logo Vinted" />
+                <Link to="/">
+                    <img src={logo} alt="Logo Vinted" />
+                </Link>
                 <div className="search">
                     <HiMagnifyingGlass className="magnifying-glass" />
                     <input type="text" name="search" value={search} onChange={event => setSearch(event.target.value)} placeholder="Recherche des articles" />
