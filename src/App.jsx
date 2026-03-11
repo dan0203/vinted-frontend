124f5d99fca3f75e4859773df23a2234e3448f66
Dan Zerbib <danzerbib@gmail.com>
2026-03-11 17:18:34 +0100

    .env file + retrieving data using axios and useEffect + display loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
 import './App.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router';
+import axios from 'axios';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Offer from './pages/Offer';
 
 function App() {
     const [search, setSearch] = useState('');
+    const [offers, setOffers] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
 
-    return (
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(import.meta.env.VITE_API_URL);
+
+                setOffers(response.data.offers);
+                setIsLoading(false);
+            } catch (error) {
+                error.message && console.log(error.message);
+                error.response && console.log(error.response.data);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    return isLoading ? (
+        <p className="loading">Chargement en cours...</p>
+    ) : (
         <>
             <Router>
                 <Header search={search} setSearch={setSearch} />
