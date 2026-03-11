0bb85ae0f78596619e09a6b55e70dbdb16dc4657
Dan Zerbib <danzerbib@gmail.com>
2026-03-11 22:31:47 +0100

    added aside product card in individual offer page

--- src/pages/Offer.jsx
+++ src/pages/Offer.jsx
@@ -14,7 +14,7 @@ const Offer = ({ offers }) => {
     const productDetails = [];
 
     for (let i = 0; i < offer.product_details.length; i++) {
-        productDetails.push([[Object.entries(offer.product_details[i])[0][0], Object.entries(offer.product_details[i])[0][1]]]);
+        productDetails.push([Object.entries(offer.product_details[i])[0][0], Object.entries(offer.product_details[i])[0][1]]);
     }
 
     return (
@@ -22,15 +22,17 @@ const Offer = ({ offers }) => {
             <div className="container">
                 <img src={offer.product_image.url} alt={offer.product_name} />
                 <aside>
-                    <p>{offer.product_price} €</p>
-                    {productDetails.map(p => (
-                        <div key={`${p[0]} ${p[1]}`}>
-                            <p>{p[0]}</p>
-                            <p>{p[1]}</p>
-                        </div>
-                    ))}
-                    <p>{offer.product_name}</p>
-                    <p>{offer.product_description}</p>
+                    <p className="product_price">{offer.product_price} €</p>
+                    <div className="product_details_wrapper">
+                        {productDetails.map(p => (
+                            <div key={`${p[0]} ${p[1]}`} className="product_details">
+                                <p className="product_details_key">{p[0]}</p>
+                                <p className="product_details_value">{p[1]}</p>
+                            </div>
+                        ))}
+                    </div>
+                    <p className="product_name">{offer.product_name}</p>
+                    <p className="product_description">{offer.product_description}</p>
                     <p className="user-info">
                         <img src={offer.owner.account.avatar.url} alt={offer.owner.account.username} />
                         <span>{offer.owner.account.username}</span>
