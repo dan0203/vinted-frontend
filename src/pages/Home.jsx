5d788aea5db85c3ea12864abe6ed4af7ca41bbaf
Dan Zerbib <danzerbib@gmail.com>
2026-03-11 16:55:47 +0100

    added hero section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,16 @@
 const Home = () => {
-    return <div>Page d'accueil : Home</div>;
+    return (
+        <main>
+            <div className="hero">
+                <div className="container">
+                    <div className="highlight">
+                        <h1>Prêts à faire du tri dans vos placards ?</h1>
+                        <button>Commencer à vendre</button>
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
 };
 
 export default Home;
