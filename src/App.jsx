32b67aa88a11f9b5742c72db662ec2611d6efe04
Dan Zerbib <danzerbib@gmail.com>
2026-03-12 18:22:02 +0100

    Added login functionality with cookie

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Header from './components/Header/Header';
 import Home from './pages/Home/Home';
 import Offer from './pages/Offer/Offer';
 import Signup from './pages/Signup/Signup';
+import Login from './pages/Login/Login';
 
 function App() {
     const [search, setSearch] = useState('');
@@ -16,6 +17,7 @@ function App() {
                 <Routes>
                     <Route path="/" element={<Home />} />
                     <Route path="/signup" element={<Signup />} />
+                    <Route path="/login" element={<Login />} />
                     <Route path="/offers/:id" element={<Offer />} />
                 </Routes>
             </Router>
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,76 @@
+import './Login.css';
+import { useState } from 'react';
+import axios from 'axios';
+import Cookies from 'js-cookie';
+import { Link, useNavigate } from 'react-router';
+
+const Login = () => {
+    const navigate = useNavigate();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+
+    return (
+        <>
+            <main className="main-login">
+                <div className="container">
+                    <h1>Se connecter</h1>
+                    {error && <p className="error">{error}</p>}
+                    <form
+                        onSubmit={async event => {
+                            event.preventDefault();
+
+                            try {
+                                const data = {
+                                    email,
+                                    password,
+                                };
+
+                                // 1 : requête bdd ajout utilisateur
+                                const response = await axios.post(import.meta.env.VITE_API_URL + '/user/login', data);
+
+                                // 2 : si la réponse est ok, stocker le token dans un cookie
+                                Cookies.set('token', response.data.token, { expires: 7 });
+
+                                // 3 : rediriger vers Home
+                                navigate('/');
+                            } catch (error) {
+                                error.message && console.log('error.message', error.message);
+                                error.response && console.log('error.response.data', error.response.data);
+                                setError(error.response.data.message);
+
+                                // si l'utilisateur n'existe pas, rediriger vers /signup
+                                if (error.status === 400) {
+                                    navigate('/signup');
+                                }
+                            }
+                        }}
+                    >
+                        <input
+                            type="email"
+                            name="email"
+                            placeholder="Email"
+                            value={email}
+                            onChange={event => {
+                                setEmail(event.target.value);
+                            }}
+                        />
+                        <input
+                            type="password"
+                            name="password"
+                            placeholder="Mot de passe"
+                            value={password}
+                            onChange={event => {
+                                setPassword(event.target.value);
+                            }}
+                        />
+                        <button>Se connecter</button>
+                    </form>
+                    <Link to="/signup">Pas encore de compte ? Inscris-toi !</Link>
+                </div>
+            </main>
+        </>
+    );
+};
+
+export default Login;
